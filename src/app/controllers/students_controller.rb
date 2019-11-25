64fc0f4b6dad31e1f3b975f0a3db72ddef31880e
System Administrator <sysadmin@hpc268.tcnj.edu>
2019-11-24 23:15:12 -0500

    accounts system and searching algorithm updated

--- src/app/controllers/students_controller.rb
+++ src/app/controllers/students_controller.rb
@@ -10,6 +10,7 @@ class StudentsController < ApplicationController
   # GET /students/1
   # GET /students/1.json
   def show
+    @mentors = Mentor.text_search(params[:query]).page(params[:page]).per_page(5)
   end
 
   # GET /students/new
--- src/app/models/mentor.rb
+++ src/app/models/mentor.rb
@@ -1,3 +1,18 @@
 class Mentor < ApplicationRecord
+    # class relationships -> this establishes foreign_key for user account
     belongs_to :user_id, class_name: 'User'
+
+    # define "fullname"
+    def fullname
+        return self.firstName + " " + self.lastName
+    end
+
+    # algorithm for full text searching
+    def self.text_search(query)
+        if query.present?
+            where("\"firstName\" @@ :q or \"lastName\" @@ :q or \"bio\" @@ :q or \"collegeName\" @@ :q or \"specialty\" @@ :q or \"collegeYear\" @@ :q or \"major\" @@ :q", q: query)
+        else
+            all
+        end
+    end
 end
